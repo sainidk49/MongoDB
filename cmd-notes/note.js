d0e98d337e16aec00006bf4370ab08827cfc0e20
Deepak Saini <dpksaini49@gmail.com>
2024-08-30 18:31:15 +0530

    add commonds in nite

---
+++ cmd-notes/note.js
@@ -0,0 +1,17 @@
+// #1 =>  show dbs  :- show all database
+
+// #2 => show collectiosn  :- show all collectios
+
+// #3 => use mydatabase  :- use your database
+
+// #4 => db.mycollection.insertOne({"name":"Deepak", "age":27, "gmail": "dpksaini49@gmail.com"})
+
+// #5 => db.mycollection.insertMany([{"name":"Deepak", "age":27, "gmail": "dpksaini49@gmail.com"}, {"name":"ajay", "age":23, "gmail": "ajbist@gmail.com"}])
+
+// #6 => db.mycollection.findOne({"name":"Deepak"})
+
+// #7 => db.mycollection.find({"name":"Deepak"})  :- provide first data 
+
+// #8 => db.mycollection.find({"name":"Deepak"})  :- provide first data 
+
+// #9 => db.mycollection.fine({"name":"Deepak"}, {"name":1, "age": 1})  :- provide only these data
